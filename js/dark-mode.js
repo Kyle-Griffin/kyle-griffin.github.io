728cd24896c35ed2227c6db563afcd795b89f793
Kyle-Griffin <KyleFileGriffin@gmail.com>
2022-01-12 19:08:52 +0000

    Update crucible link

--- js/dark-mode.js
+++
@@ -1,5 +0,0 @@
-let darkModeToggle = document.querySelector(".toggle-slot");
-let bodyClasses = document.querySelector("body");
-darkModeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
